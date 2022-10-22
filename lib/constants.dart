f5dbe73094f689874d81e61d8f07a09d9b364b75
Lahiru-Ima <lahiru.sl24@gmail.com>
2022-10-22 15:27:48 +0530

    Add constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,12 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF009999);
 const kPrimaryLightColor = Color.fromARGB(255, 168, 235, 235);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomLeft,
+  colors: [Color.fromARGB(255, 147, 240, 248),Color(0xFF009999)]
+);
+const kSecondaryColor = Color(0xFF979797);
+const kTextColor = Color(0xFF757575);
+
+const kAnimationDuration = Duration(milliseconds: 200);
