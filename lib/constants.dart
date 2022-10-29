03cc52b0b0239b4efb2d528e7f154877c2a70289
Lahiru-Ima <lahiru.sl24@gmail.com>
2022-10-29 10:42:47 +0530

    Add shadow color as const

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xFF009999);
 const kPrimaryLightColor = Color.fromARGB(255, 168, 235, 235);
 const kPrimaryGradientColor = LinearGradient(
-  begin: Alignment.topLeft,
-  end: Alignment.bottomLeft,
-  colors: [Color.fromARGB(255, 147, 240, 248),Color(0xFF009999)]
-);
+    begin: Alignment.topLeft,
+    end: Alignment.bottomLeft,
+    colors: [Color.fromARGB(255, 147, 240, 248), Color(0xFF009999)]);
 const kSecondaryColor = Color(0xFF979797);
 const kTextColor = Color(0xFF757575);
 
 const kAnimationDuration = Duration(milliseconds: 200);
+
+final kShadowColor = const Color(0xFFD3D3D3).withOpacity(.84);
