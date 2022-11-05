9ac93a9703c12a6df9b71a2b845c1bd66848b028
Lahiru-Ima <lahiru.sl24@gmail.com>
2022-11-05 23:17:28 +0530

    Add rounded password field

---
+++ lib/components/rounded_password_field.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+import 'text_field_container.dart';
+
+class RoundedPasswordField extends StatelessWidget {
+  final String hintText;
+  final ValueChanged<String> onChanged;
+  const RoundedPasswordField({
+    Key? key,
+    required this.onChanged,
+    required this.hintText,
+  }) : super(key: key);
+
+// ignore_for_file: prefer_const_constructors
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+      child: TextField(
+        obscureText: true,
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          hintText: hintText,
+          icon: Icon(
+            Icons.lock,
+            color: kPrimaryColor,
+          ),
+          suffixIcon: Icon(
+            Icons.visibility,
+            color: kPrimaryColor,
+          ),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
