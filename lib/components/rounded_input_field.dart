63f717d2cbeedf980fe6538b280535101ece6e21
Lahiru-Ima <lahiru.sl24@gmail.com>
2022-11-05 23:17:06 +0530

    Add rounded input field

---
+++ lib/components/rounded_input_field.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import 'text_field_container.dart';
+
+class RoundedInputField extends StatelessWidget {
+  final String hintText;
+  final IconData icon;
+  final ValueChanged<String> onChanged;
+  const RoundedInputField({
+    Key? key,
+    required this.hintText,
+    this.icon = Icons.person,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+      child: TextField(
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          icon: Icon(
+            icon,
+            color: kPrimaryColor,
+          ),
+          hintText: hintText,
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
