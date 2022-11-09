936caa1a9225b06e71aafa4acc8288933c95075a
Lahiru-Ima <lahiru.sl24@gmail.com>
2022-11-09 12:06:34 +0530

    Add question controller

---
+++ lib/controllers/question_controller.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_final_fields, unnecessary_this
+
+import 'package:get/get.dart';
+import '../screens/models/Questions.dart';
+
+class QuestionController extends GetxController {
+  List<Question> _questions = sample_data
+      .map(
+        (question) => Question(
+            id: question['id'],
+            question: question['question'],
+            options: question['options']),
+      )
+      .toList();
+
+  List<Question> get questions => this._questions;
+}
