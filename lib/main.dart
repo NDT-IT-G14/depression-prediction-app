d522373e69d647942ac95ebb6236a124c6c7259b
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-11-23 13:46:05 +0530

    eddit main file

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,30 @@ import 'package:depression_prediction_app/constants.dart';
 import 'package:depression_prediction_app/routes.dart';
 import 'package:depression_prediction_app/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // We're using the manual installation on non-web platforms since Google sign in plugin doesn't yet support Dart initialization.
+  // See related issue: https://github.com/flutter/flutter/issues/96391
+  if (false) {
+    await Firebase.initializeApp();
+  } else {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyD1To6MJRV4oJjj6j5M02h89PC_6rV3rbI",
+          authDomain: "quakka-1-1.firebaseapp.com",
+          projectId: "quakka-1-1",
+          storageBucket: "quakka-1-1.appspot.com",
+          messagingSenderId: "291233092534",
+          appId: "1:291233092534:web:f36f5b0267891ce8f7e717"),
+    );
+  }
+
+  // if (shouldUseFirebaseEmulator) {
+  //   await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  // }
+
   runApp(MyApp());
 }
 
