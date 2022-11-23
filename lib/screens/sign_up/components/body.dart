ce4e0c08ebfb165201cb96c6c9c3812604d2dd14
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-11-23 13:49:20 +0530

    edit authentication and sigin

--- lib/screens/sign_up/components/body.dart
+++ lib/screens/sign_up/components/body.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:depression_prediction_app/screens/home/home_screen.dart';
 import 'package:depression_prediction_app/screens/sign_in/sign_in_screen.dart';
 import 'package:flutter/material.dart';
 import '../../../components/already_have_an_account_check.dart';
@@ -9,8 +10,12 @@ import '../../../components/rounded_password_field.dart';
 import '../../../components/social_icon.dart';
 import '../../../constants.dart';
 import 'or_divider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Body extends StatelessWidget {
+  String email = '';
+  String password = '';
+  String confirm_password = '';
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -32,27 +37,45 @@ class Body extends StatelessWidget {
         ),
         RoundedInputField(
           hintText: "Your Email",
-          onChanged: (value) {},
+          onChanged: (value) {
+            email = value;
+          },
         ),
         RoundedPasswordField(
           hintText: "Password",
-          onChanged: (value) {},
+          onChanged: (value) {
+            password = value;
+          },
         ),
         RoundedPasswordField(
           hintText: "Confirm Password",
-          onChanged: (value) {},
-        ),
-        SizedBox(height: 10),
-        RoundedButton(
-          text: "SIGNUP",
-          press: () {
-            Navigator.of(context).pushNamed(SignInScreen.routeName);
+          onChanged: (value) {
+            confirm_password = value;
           },
         ),
         SizedBox(height: 10),
+        RoundedButton(
+            text: "SIGNUP",
+            press: () async {
+              if (password == confirm_password) {
+                await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                    email: email, password: password);
+                Navigator.of(context).pushNamed(HomeScreen.routeName);
+              } else {
+                print('$password and $confirm_password does not match');
+              }
+            }
+            // press: () {
+            //   Navigator.of(context).pushNamed(SignInScreen.routeName);
+            // },
+            ),
+        SizedBox(height: 10),
         AlreadyHaveAnAccountCheck(
           login: false,
-          press: () {},
+          press: () {
+            print('fire');
+            // Navigator.of(context).pushNamed(SignInScreen.routeName);
+          },
         ),
         OrDivider(),
         Row(
@@ -74,5 +97,16 @@ class Body extends StatelessWidget {
         )
       ],
     );
+  }
+
+  Future SignUP() async {
+    print('fire on...');
+    if (password == confirm_password) {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      print('signup');
+    } else {
+      print('$password and $confirm_password does not match');
+    }
   }
 }
