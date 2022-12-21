56ffa719c92afe27fd5207fe1b4a549b241f980c
Lahiru-Ima <lahiru.sl24@gmail.com>
2022-12-21 13:11:34 +0530

    Fix home screen top

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,6 +2,7 @@
 
 import 'package:depression_prediction_app/constants.dart';
 import 'package:depression_prediction_app/screens/feedback/feedback_screen.dart';
+import 'package:depression_prediction_app/screens/home/components/bottom_nav_bar.dart';
 import 'package:depression_prediction_app/screens/profile/profile_screen.dart';
 import 'package:depression_prediction_app/screens/quiz/quiz_screen.dart';
 import 'package:flutter/material.dart';
@@ -13,83 +14,128 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
+    return Scaffold(
+      body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 20),
+        children: <Widget>[
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 25),
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            height: 240,
             width: double.infinity,
-            height: 150,
             decoration: BoxDecoration(
-              color: kPrimaryColor,
-              borderRadius: BorderRadius.circular(29),
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: const [
+                  kPrimaryColor,
+                  kSecondaryColor,
+                ],
+              ),
+              // image: const DecorationImage(
+              //   image: AssetImage("assets/images.png"),
+              //   fit: BoxFit.fitWidth,
+              // ),
             ),
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).pushNamed(QuizScreen.routeName);
-              },
-              child: Text.rich(
-                TextSpan(
-                  text: "Start symtoms monitoring",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+            child: SafeArea(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        "Check Your Mental Health\nWithout Going Anyware",
+                        style: TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamed(QuizScreen.routeName);
+                        },
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: kShadowColor),
+                            ),
+                          ),
+                        ),
+                        child: const Text('Start quiz'),
+                      ),
+                    ],
                   ),
-                ),
+                  // Image(
+                  //   image: AssetImage(
+                  //     "",
+                  //   ),
+                  //   fit: BoxFit.fitHeight,
+                  //   alignment: Alignment.bottomRight,
+                  // ),
+                ],
               ),
             ),
           ),
-          SizedBox(height: 20),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  ListCard(
-                    text: 'Profile',
-                    subtext: "Sea your profile",
-                    press: () {
-                      Navigator.of(context).pushNamed(ProfileScreen.routeName);
-                    },
-                  ),
-                  ListCard(
-                    text: 'Quotes',
-                    subtext: "Fulfill your life",
-                    press: () {
-                      Navigator.of(context).pushNamed(QuotesScreen.routeName);
-                    },
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  ListCard(
-                    text: 'Emergancy',
-                    subtext: "Enjoy your life",
-                    press: () {
-                      // Navigator.of(context).pushNamed(.routeName);
-                    },
-                  ),
-                  ListCard(
-                    text: 'Feedbacks',
-                    subtext: "Fulfill your life",
-                    press: () {
-                      Navigator.of(context).pushNamed(FeedbackScreen.routeName);
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
+          SizedBox(height: 10),
+          Card(context),
+          // BottomNavBar(),
         ],
       ),
+    );
+  }
+
+  Column Card(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            ListCard(
+              text: 'Profile',
+              subtext: "Sea your profile",
+              press: () {
+                Navigator.of(context).pushNamed(ProfileScreen.routeName);
+              },
+            ),
+            ListCard(
+              text: 'Quotes',
+              subtext: "Fulfill your life",
+              press: () {
+                Navigator.of(context).pushNamed(QuotesScreen.routeName);
+              },
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            ListCard(
+              text: 'Emergancy',
+              subtext: "Enjoy your life",
+              press: () {
+                // Navigator.of(context).pushNamed(.routeName);
+              },
+            ),
+            ListCard(
+              text: 'Feedbacks',
+              subtext: "Fulfill your life",
+              press: () {
+                Navigator.of(context).pushNamed(FeedbackScreen.routeName);
+              },
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
