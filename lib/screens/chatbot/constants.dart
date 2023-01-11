7013b3f357ce7222fbc20a2e76e9bfc6b972c6f4
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2023-01-11 12:06:10 +0530

    edit feedback screen chneg colors and images

--- lib/screens/chatbot/constants.dart
+++ lib/screens/chatbot/constants.dart
@@ -3,3 +3,5 @@
 // ?]const apiSecretKey = "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";
 
 const apiSecretKey = "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";
+
+// const apiSecretKey ="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"
